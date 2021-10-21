d3e0f69cd4076f44aaf1044d587f63abe78d2993
AdagiosByte <43786133+AdagiosByte@users.noreply.github.com>
2021-10-20 21:06:11 -0400

    Add files via upload
    This version introduces the following features:
    - Clicks on and buys upgrades
    - Buys companies
    - Uses increase funding exploit
    
    Note:
    - Tested on Opera Browser
    - Needs managers to be bought prior to use

---
+++ adv.py
@@ -0,0 +1,41 @@
+import pyautogui
+import time
+
+
+while True:
+	time.sleep(7)
+	pyautogui.click(400, 500)
+	time.sleep(2)
+	pyautogui.press('enter')
+	time.sleep(28) #load time
+	pyautogui.doubleClick(1280, 250) #dollar tag
+
+	#buy upgrades
+	pyautogui.click(200, 550) #upgrades
+	time.sleep(0.5)
+	pyautogui.click(1100, 750) #quick buy
+	time.sleep(0.05)
+	pyautogui.doubleClick(1280, 270, 0.25) #close menu
+	time.sleep(0.05)
+
+	#buy companies
+	#pyautogui.doubleClick(1280, 250) #dollar tag
+	pyautogui.click(700, 520) #buy newspapers
+	pyautogui.click(700, 640) #buy carwashs
+	pyautogui.click(700, 760) #buy pizzas
+	pyautogui.click(700, 880) #buy donuts
+	pyautogui.click(700, 400) #buy lemons
+	pyautogui.click(1100, 880) #buy oil rigs
+	pyautogui.click(1100, 760) #buy banks
+	pyautogui.click(1100, 640) #buy movie studios
+	pyautogui.click(1100, 520) #buy hockey teams
+	pyautogui.click(1100, 400) #buy shrimp
+
+	#crash game
+	pyautogui.click(700, 950) #launch
+	time.sleep(0.5)
+	pyautogui.click(700, 900) #increase funds
+	time.sleep(0.5)
+	pyautogui.keyDown('ctrl')
+	pyautogui.press('r')
+	pyautogui.keyUp('ctrl')
